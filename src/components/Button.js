3d6d9e0255937f54625e40f5f075ff24d90f2c01
Joe Edwards <Joe.Edwards.SAV@gmail.com>
2020-03-05 16:12:21 -0500

    adjusts ui for login

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,11 +4,24 @@ import { StyleSheet, View, Text, Container } from 'react-native';
 class Button extends Component {
   render() {
     return (
-      <View>
+      <View style={styles.button}>
       <Text>{this.props.name}</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  button: {
+    height: 44,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 150,
+    margin: 8,
+    borderRadius: 10,
+  }
+
+});
 
 export default Button;
