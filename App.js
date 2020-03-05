8b0a9810fcfc9e84f28baa928cf01f8d0600a41f
Joe Edwards <Joe.Edwards.SAV@gmail.com>
2020-03-05 18:10:15 -0500

    adds navigation contaainer; refactoring

--- App.js
+++ App.js
@@ -1,23 +1,50 @@
 import React, {
   Component
 } from 'react';
-import { View, Text } from 'react-native';
-
-import Button from './src/components/Button.js';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default class App extends Component {
+
   render() {
     return (
-        <View style = {{
-          flex: 1,
-          backgroundColor: 'silver',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-        <Text>welcome to firebase login</Text>
-          <Button name ='login'/>
-          <Button name = 'signup'/>
+      <NavigationContainer>
+        <View style = {styles.view}>
+          <Text>welcome to firebase login</Text>
+          <Button
+            style={styles.button}
+            title = 'login'
+            onPress={this._login}
+          />
+          <Button
+            style={styles.button}
+            title = 'signup'
+            onPress={this._signUp}
+          />
         </View>
+      </NavigationContainer>
     );
   }
-}
+
+};
+
+
+const styles = StyleSheet.create({
+  button: {
+    height: 44,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 150,
+    margin: 8,
+    borderRadius: 10,
+  },
+
+  view: {
+    flex: 1,
+    backgroundColor: 'silver',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+});
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,27 +1,2 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, Container } from 'react-native';
-
-class Button extends Component {
-  render() {
-    return (
-      <View style={styles.button}>
-      <Text>{this.props.name}</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  button: {
-    height: 44,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: 150,
-    margin: 8,
-    borderRadius: 10,
-  }
-
-});
-
-export default Button;
+import { StyleSheet, View, Text, Button} from 'react-native';
