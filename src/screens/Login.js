77f044aeeb229be61f357469956e5c59f0606589
Joe Edwards <Joe.Edwards.SAV@gmail.com>
2020-03-06 14:16:19 -0500

    adds more rough ui to both login and sign up pages

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,21 +1,80 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button, TextInput, Container } from 'react-native';
+import { StyleSheet, ScrollView, View,Text, Button, TextInput} from 'react-native';
 
 export default function Login() {
-  return(
-    <View style={styles.login}>
-      <Text>Login</Text>
-    </View>
+  return (
+    <ScrollView>
+      <View style={styles.login}>
+        <View style={styles.header}>
+          <Text style={styles.label}>Welcome to{'\n\n'}JUUL!</Text>
+        </View>
+        <TextInput style={styles.email}
+          autoCompleteType='email'
+          placeholder="enter your email"
+        >
+        </TextInput>
+        <TextInput style={styles.password}
+          autoCompleteType='password'
+          placeholder="enter your password"
+          secureTextEntry={true}
+          >
+        </TextInput>
+        <View style={styles.body}>
+        </View>
+        <Button
+          style={styles.button}
+          title = "Forgot Password?">
+        </Button>
+        <Button title = "Login">
+        </Button>
+      </View>
+    </ScrollView>
   )
-
 }
 
 const styles = StyleSheet.create({
-  login: {
-    backgroundColor: 'red',
+
+  label: {
+    textAlign: 'center'
+  },
+
+  header: {
+    height: 150,
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+
+
+  },
+
+  body: {
+    height: 44
+  },
+
+  login: {
+    justifyContent: 'center',
+    alignItems: 'stretch',
     flex: 1
+  },
+
+  email: {
+    textAlign: 'center',
+    backgroundColor: 'white',
+    marginHorizontal: 16,
+    marginVertical: 8,
+    borderRadius: 4,
+    height: 44
+
+  },
+
+  password: {
+    textAlign: 'center',
+    backgroundColor: 'white',
+    marginHorizontal: 16,
+    marginVertical: 8,
+    borderRadius: 4,
+    height: 44,
+
   }
 
 });
--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,18 +1,77 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button, TextInput } from 'react-native';
+import { StyleSheet, ScrollView, View,Text, Button, TextInput} from 'react-native';
 
-export default function Login() {
-  return(
-    <View style={styles.signUp} >
-    </View>
+export default function SignUp() {
+  return (
+    <ScrollView>
+      <View style={styles.login}>
+        <View style={styles.header}>
+          <Text style={styles.label}>Enter your email and pasword to sign up!</Text>
+        </View>
+        <TextInput style={styles.email}
+          autoCompleteType='email'
+          placeholder="enter your email"
+        >
+        </TextInput>
+        <TextInput style={styles.password}
+          autoCompleteType='password'
+          placeholder="enter your password"
+          secureTextEntry={true}
+          >
+        </TextInput>
+        <View style={styles.body}>
+
+        </View>
+        <Button title = "sign up">
+        </Button>
+      </View>
+    </ScrollView>
   )
-
 }
 
 const styles = StyleSheet.create({
-  signUp: {
-    backgroundColor: 'blue',
+
+  label: {
+    textAlign: 'center'
+  },
+
+  header: {
+    height: 150,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+
+
+  },
+
+  body: {
+    height: 44
+  },
+
+  login: {
+    justifyContent: 'center',
+    alignItems: 'stretch',
     flex: 1
+  },
+
+  email: {
+    textAlign: 'center',
+    backgroundColor: 'white',
+    marginHorizontal: 16,
+    marginVertical: 8,
+    borderRadius: 4,
+    height: 44
+
+  },
+
+  password: {
+    textAlign: 'center',
+    backgroundColor: 'white',
+    marginHorizontal: 16,
+    marginVertical: 8,
+    borderRadius: 4,
+    height: 44,
+
   }
 
 });
