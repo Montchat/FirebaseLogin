fc33748e6f7fd271cad79eef395fe7bc55c70adb
Joe Edwards <Joe.Edwards.SAV@gmail.com>
2020-03-09 19:38:08 -0400

    adds firebase app container

--- App.js
+++ App.js
@@ -1,17 +1,38 @@
-import React, { Component } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { Home, Login, Main, SignUp } from './src/screens';
+import React { Component } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+import firebase from firebase;
+import Main from './src/screens/Main';
+import Home from './src/screens/Home';
+import Login from '.src/screens/Login';
 
-const AuthStack = createStackNavigator();
+const AuthContext = React.createContext();
 
-export default class App extends Component {
-  <NavigationContainer>
-    <AuthStack.Navigator>
-      <AuthStack.Screen name="Home" component={Home} />
-      <AuthStack.Screen name ="Login" component={Login} />
-      <AuthStack.Screen name ="SignUp" component={SignUp} />
-    </AuthStack.Navigator>
-  </NavigationContainer>
+const MainStack = createStackNavigator({
+  Main: Main
 
-}
+});
+
+export default class App extends React.Component {
+  state = {loggedIn: null}
+  componentDidMount() {
+    firebase.initialzeApp();
+
+    firebase.auth().onAuthStateChange((user)) => {
+      if (user) {
+        this.setState({ loggedIn: true })
+      } else {
+        this.setState({ loggedIn: false })
+      }
+    }
+  }
+};
+
+export default createAppContainer(
+  createSwitchNavigator( {
+    Loading: "Loading",
+    Home: "Home",
+    Login: "Login"
+  }, {
+    initialRoutingName: "Loading"
+  })
+);
